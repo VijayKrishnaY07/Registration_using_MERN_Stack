e60db2e192095e227455d56fd802aa453659cf66
Vijay Krishna <vijaykrishnay07@gmail.com>
2024-07-03 02:24:10 +0530

    Add Tailwind to the react project-client

--- client/src/App.js
+++ client/src/App.js
@@ -1,10 +1,7 @@
-import logo from "./logo.svg";
-import "./App.css";
-
 function App() {
   return (
     <div className="App">
-      <h1>React App</h1>
+      <h1 class="text-3xl font-bold underline">Hello world!</h1>
     </div>
   );
 }
