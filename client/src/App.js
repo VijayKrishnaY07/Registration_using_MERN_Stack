1e1623dd22acd1bfe7d2900f3f45d476ce8f3e3e
Vijay Krishna <vijaykrishnay07@gmail.com>
2024-07-03 02:46:48 +0530

    created routes for all the components

--- client/src/App.js
+++ client/src/App.js
@@ -1,8 +1,52 @@
+import React from "react";
+import { createBrowserRouter, RouterProvider } from "react-router-dom";
+
+/*imports */
+import Username from "./components/Username";
+import PageNotFound from "./components/PageNotFound";
+import Password from "./components/Password";
+import Profile from "./components/Profile";
+import Recovery from "./components/Recovery";
+import Register from "./components/Register";
+import Reset from "./components/Reset";
+
+/**routes */
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <Username></Username>,
+  },
+  {
+    path: "/profile",
+    element: <Profile></Profile>,
+  },
+  {
+    path: "/recovery",
+    element: <Recovery></Recovery>,
+  },
+  {
+    path: "/register",
+    element: <Register></Register>,
+  },
+  {
+    path: "/reset",
+    element: <Reset></Reset>,
+  },
+  {
+    path: "/*",
+    element: <PageNotFound></PageNotFound>,
+  },
+  {
+    path: "/password",
+    element: <Password></Password>,
+  },
+]);
+
 function App() {
   return (
-    <div className="App">
-      <h1 class="text-3xl font-bold underline">Hello world!</h1>
-    </div>
+    <main>
+      <RouterProvider router={router}></RouterProvider>
+    </main>
   );
 }
 
